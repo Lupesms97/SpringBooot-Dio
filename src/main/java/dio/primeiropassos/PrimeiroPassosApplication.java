958b64ee168e989432fb6c9692c64345025a6a2d
Lupesms97 <lupesms97@gmail.com>
2022-12-03 01:14:03 -0300

    Classe calculadora criada injeção feita

--- src/main/java/dio/primeiropassos/PrimeiroPassosApplication.java
+++ src/main/java/dio/primeiropassos/PrimeiroPassosApplication.java
@@ -8,6 +8,13 @@ public class PrimeiroPassosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroPassosApplication.class, args);
+
+		/**Não é possivel fazer o uso do new
+		pois é implememntado na classe myapp o metodo run
+		para que toda vez que a aplicação iniciar ele estara como dependicia e rodara
+		**/
+		//Calculadora calculadora = new Calculadora();
+
 	}
 
 }
